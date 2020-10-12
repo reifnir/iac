bd4af76485c6faf788f37bec31eba00a97350ab5
Jim Andreasen <reifnir@gmail.com>
2020-10-11 23:31:54 -0400

    hide that from being logged

--- azure/modules/gitlab_variables/group_variables.tf
+++ azure/modules/gitlab_variables/group_variables.tf
@@ -1,7 +1,7 @@
 resource "gitlab_group_variable" "kubernetes_provider_info" {
    group     = data.gitlab_group.all_projects.id
-   key       = "TF_VAR_kubernetes_provider_info"
-   value     = jsonencode(var.kubernetes_provider_info)
+   key       = "TF_VAR_kubernetes_provider_info_encoded"
+   value     = base64encode(jsonencode(var.kubernetes_provider_info))
    protected = false
-   masked    = false
+   masked    = true
 }
