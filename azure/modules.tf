baced6c85a92e5c39d6270397ab5471262748bf6
Jim Andreasen <reifnir@gmail.com>
2020-10-17 11:48:12 -0400

    double nodes, this is super cheap

--- azure/modules.tf
+++ azure/modules.tf
@@ -4,7 +4,7 @@ module "aks" {
   resource_group = azurerm_resource_group.cluster
 
   aks_cluster_version = "1.19.0"
-  aks_node_count      = 1
+  aks_node_count      = 2
   aks_vm_node_size    = "Standard_B2s"
 
   aks_admin_username        = var.aks_admin_username
