d9b636ad1e40d9ed90dd47664b70327773a245a1
Jim Andreasen <reifnir@gmail.com>
2020-12-27 12:40:26 -0500

    fixed directory

--- scripts/format.sh
+++ scripts/format.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-set -ex
+set -e
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-find "$SCRIPT_DIR" -name "*.tf" -execdir terraform fmt {} \;
+find "$SCRIPT_DIR/.." -name "*.tf" -execdir terraform fmt {} \;
