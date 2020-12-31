356781d73b861b9f5945a4608ea3ca3d73f6e141
Jim Andreasen <reifnir@gmail.com>
2020-12-31 10:03:58 -0500

    don't leave the script until the created workloads are ready

--- scripts/install-aad-pod-identity.sh
+++ scripts/install-aad-pod-identity.sh
@@ -1,6 +1,26 @@
 #!/bin/bash
 set -e
 
+function wait-for-daemonset {
+  DAEMONSET="$1"
+  NAMESPACE="$2"
+
+  echo "Waiting for up to 60 seconds for daemonset '$DAEMONSET' in namespace '$NAMESPACE' to be available..."
+  RETRIES=20
+  while [[ $RETRIES -ge 0 ]];do
+    sleep 3
+    READY=$(kubectl -n $DAEMONSET get daemonset $NAMESPACE -o jsonpath="{.status.numberReady}")
+    DESIRED=$(kubectl -n $DAEMONSET get daemonset $NAMESPACE -o jsonpath="{.status.desiredNumberScheduled}")
+    echo "  Count desired/ready: $DESIRED/$READY"
+    if [[ $READY -eq $DESIRED ]];then
+      echo "  Daemonset successfully started."
+      break
+    fi
+    ((RETRIES--))
+  done
+}
+
+
 echo "Setting paths..."
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 MANIFEST_PATH="$SCRIPT_DIR/../files/aad-pod-identity.yaml"
@@ -20,3 +40,8 @@ else
   kubectl replace -f "$MANIFEST_PATH"
 fi
 
+echo "Wait until the Node Managed Identity daemonset is ready..."
+wait-for-daemonset "nmi" "default"
+
+echo "Wait until the Managed Identity Controller deployment is ready..."
+kubectl wait --for=condition=available --timeout=60s deployment mic
