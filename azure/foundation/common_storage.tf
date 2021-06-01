fbe1966c69abdb0e28af8973b7b1467926a1b2b8
Jim Andreasen <jim.andreasen@reifnir.com>
2021-05-31 21:38:13 -0400

    don't delete state

---
+++ azure/foundation/common_storage.tf
@@ -0,0 +1,23 @@
+resource "azurerm_resource_group" "common_storage" {
+  name     = "rg-common-storage"
+  location = var.location
+  tags     = local.tags
+}
+
+# azurerm_storage_account.common will be destroyed
+resource "azurerm_storage_account" "common" {
+  name                = "sareifnircommonstorage"
+  resource_group_name = azurerm_resource_group.common_storage.name
+  location            = var.location
+
+  account_replication_type = "LRS"
+  account_tier             = "Standard"
+  min_tls_version          = "TLS1_2"
+
+  tags = local.tags
+}
+
+ resource "azurerm_storage_container" "terraform_state" {
+     name                    = "terraform-state"
+     storage_account_name    = "sareifnircommonstorage" 
+  }
